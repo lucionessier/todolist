3dfbca98a7690706b920c81456532f6fc16b02d3
LucioNessier <nessier.lucio@gmail.com>
2021-09-06 19:57:52 -0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 // import TodoForm from "./components/TodoForm";
 import TodoList from './components/TodoList';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <>
     <div className="todo-app">
       <TodoList/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p className="copy">Developed with love by <a href="https://nessierdg.netlify.app/" target="_blank">Lucio Nessier</a> © . See <a href="https://github.com/lucionessier" target="_blank">full repo</a>. </p>
+        </footer>
+    )
+}
+
+export default Footer
