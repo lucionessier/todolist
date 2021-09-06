117f3bc3b74ce8235d463d50969806f24d3c3ac4
LucioNessier <nessier.lucio@gmail.com>
2021-09-06 20:06:48 -0300

    Used noreferrer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            <p className="copy">Developed with love by <a href="https://nessierdg.netlify.app/" target="_blank">Lucio Nessier</a> © . See <a href="https://github.com/lucionessier" target="_blank">full repo</a>. </p>
+            <p className="copy">Developed with love by <a href="https://nessierdg.netlify.app/" rel="noreferrer" target="_blank">Lucio Nessier</a> © . See <a href="https://github.com/lucionessier" target="_blank" rel="noreferrer">full repo</a>. </p>
         </footer>
     )
 }
